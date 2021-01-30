44114daa5c23a91f9c134bad33eb88d31f213975
DavidBorie <72392653+DavidBorie@users.noreply.github.com>
2021-01-30 21:12:23 +0100

    Update TableauTest.js

--- src/scenes/TableauTest.js
+++ src/scenes/TableauTest.js
@@ -4,6 +4,10 @@ class Tableau00 extends Tableau{
         super.preload();
         this.load.image('monster-violet', 'assets/monster-violet.png');
         this.load.image('monster-fly', 'assets/monster-fly.png');
+        this.load.image('monster-fix', 'assets/monster-fix.png');
+        this.load.image('monster-jump', 'assets/monster-jump.png');
+        this.load.image('monster-fast', 'assets/monster-fast.png');
+        //1.monstre volant 2.monstre mouvant 3.Monstre immobile 4. Monstre sautant  5.Monstre Rapide
     }
     create() {
         super.create();
@@ -20,13 +24,41 @@ class Tableau00 extends Tableau{
         this.star3=this.physics.add.sprite(600,100,"star");
         this.star3.setCollideWorldBounds(true);
         this.star3.setBounce(1);
+        
 
-          //monstre terrestre
+          //monstre mouvant
         this.monstre=this.physics.add.sprite(300,this.sys.canvas.height-70,"monster-violet");
         this.monstre.setOrigin(0,0);
         this.monstre.setDisplaySize(100,100);
         this.monstre.setCollideWorldBounds(true);
         this.monstre.setBounce(1);
+        this.monstre.setVelocityX(50);
+        this.physics.add.overlap(this.player, this.monstre, this.hitSpike, null, this);
+        
+        //monstre immbole
+        this.monstre=this.physics.add.sprite(300,this.sys.canvas.height-70,"monster-violet");
+        this.monstre.setOrigin(0,0);
+        this.monstre.setDisplaySize(200,200);
+        this.monstre.setCollideWorldBounds(true);
+        this.monstre.setBounce(0);
+        this.monstre.setVelocityX(50);
+        this.physics.add.overlap(this.player, this.monstre, this.hitSpike, null, this);
+        
+        //monstre sautant
+        this.monstre=this.physics.add.sprite(300,this.sys.canvas.height-70,"monster-violet");
+        this.monstre.setOrigin(0,0);
+        this.monstre.setDisplaySize(120,120);
+        this.monstre.setCollideWorldBounds(true);
+        this.monstre.setBounce(6);
+        this.monstre.setVelocityX(50);
+        this.physics.add.overlap(this.player, this.monstre, this.hitSpike, null, this);
+        
+        //monstre rapide
+        this.monstre=this.physics.add.sprite(300,this.sys.canvas.height-70,"monster-violet");
+        this.monstre.setOrigin(0,0);
+        this.monstre.setDisplaySize(80,80);
+        this.monstre.setCollideWorldBounds(true);
+        this.monstre.setBounce(0);
         this.monstre.setVelocityX(50);
         this.physics.add.overlap(this.player, this.monstre, this.hitSpike, null, this);
         
