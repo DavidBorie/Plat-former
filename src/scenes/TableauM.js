48b5895f193412e012220f1a29faad04fe637323
DavidBorie <72392653+DavidBorie@users.noreply.github.com>
2021-02-01 14:21:54 +0100

    Update TableauM.js

--- src/scenes/TableauM.js
+++ src/scenes/TableauM.js
@@ -2,12 +2,27 @@ class TableauM extends Tableau{
 
     preload() {
         super.preload();
+        this.load.image('star', 'assets/star.png');
         this.load.image('monster-violet', 'assets/monster-violet.png');
-        //1.monstre volant 2.monstre mouvant 3.Monstre immobile 4. Monstre sautant  5.Monstre Rapide
+
     }
     create() {
         super.create();
-        
+        //quelques étoiles
+        let largeur=64*2;
+        this.stars=this.physics.add.group();
+        for(let posX=largeur/2;posX<largeur*7;posX+=largeur){
+            this.stars.create(posX ,0,"star");
+        }
+        this.stars.children.iterate(function (child) {
+            child.setBounce(1);
+            child.setGravity(1);
+            child.setCollideWorldBounds(true);
+            child.setVelocity( 0,Phaser.Math.Between(-100, 100));
+            child.setMaxVelocity(0,500);
+        });
+        this.physics.add.overlap(this.player, this.stars, this.ramasserEtoile, null, this);
+
         //notre monstre
         this.monstre=this.physics.add.sprite(300,this.sys.canvas.height-70,"monster-violet");
         this.monstre.setOrigin(0,0);
