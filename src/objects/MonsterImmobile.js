2edfa9f068913eedf299799c411519796ac40cd6
DavidBowieRie <72392653+DavidBorie@users.noreply.github.com>
2021-03-07 21:10:46 +0100

    Update MonsterImmobile.js

--- src/objects/MonsterImmobile.js
+++ src/objects/MonsterImmobile.js
@@ -10,7 +10,10 @@ class Monsterimmobile extends ObjetEnnemi{
         //pas de gravité
         this.body.allowGravity=false;
  
- // X
+        //gestion de la taille...car attention notre png est très grand (et c'est maaaaal car pas optimisé)
+        this.setDisplaySize(64,64);
+        
+        // X
         this.originalX=x;
         this.minX=x-200;
         this.maxX=x+200;
