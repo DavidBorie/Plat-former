045f9b9adc306cda4265d837ca02c0e241476cd1
DavidBowieRie <72392653+DavidBorie@users.noreply.github.com>
2021-03-07 11:17:54 +0100

    Update MonsterMouvant.js

--- src/objects/MonsterMouvant.js
+++ src/objects/MonsterMouvant.js
@@ -1,4 +1,4 @@
- class MonsterMouvant extends ObjetEnnemi{
+class MonsterSol extends ObjetEnnemi{
     /**
      *
      * @param {Tableau} scene
@@ -6,19 +6,22 @@
      * @param y
      */
     constructor(scene, x, y) {
-        super(scene, x, y, "MonsterMouvant");
+        super(scene, x, y, "oni");
         //pas de gravité
         this.body.allowGravity=false;
- 
- // X
+        this.setCollideWorldBounds(true);
+        this.setBodySize(this.body.width-20,this.body.height-15);
+//définir les propriété que l'on va utiliser dans notre animation
+
+        // X
         this.originalX=x;
-        this.minX=x-200;
-        this.maxX=x+200;
+        this.minX=x-20;
+        this.maxX=x+650;
 
         // Y
         this.originalY=y;
-        this.minY=y-20;
-        this.maxY=height;
+        this.minY=y -64;
+        this.maxY=height-128;
 
         // on applique les propriété du début de l'animation
         this.x=this.minX;
@@ -26,5 +29,49 @@
         this.alpha=0;
         let me=this;
 
+        //on fait apparaitre notre objet avec un petit delay, puis on lance l'animation
+        //ceci a pour effet de décaler les animations pour ce même objet
+        scene.tweens.add({
+                targets:this,
+                duration:200,
+                delay:Math.random()*3000,
+                alpha:{
+                    startDelay:Math.random()*5000,
+                    from:0,
+                    to:1,
+                },
+                onComplete: function () {
+                    me.start();
+                }
+            })
+
     }
- }
+
+    start(){
+        this.scene.tweens.add({
+            targets: this,
+            x: {
+                from: this.minX,
+                to:this.maxX,
+                duration: 5000,
+                ease: 'Sine.easeInOut',
+                yoyo: -1,
+                repeat:-1
+               
+            }
+        });
+    }
+    update(){
+        
+        if(this.body){
+            if(this.body.velocity.x<0){
+                this.flipX=false;
+            }else{
+                this.flipX=true;
+            }
+        }
+
+    }
+
+
+}
