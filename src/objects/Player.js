f605f5a85c9776bdd1df77a5d651179024ad9961
DavidBowieRie <72392653+DavidBorie@users.noreply.github.com>
2021-05-06 19:34:03 +0200

    Update Player.js

--- src/objects/Player.js
+++ src/objects/Player.js
@@ -9,7 +9,7 @@ class Player extends Phaser.Physics.Arcade.Sprite{
         this.setGravityY(700)
         this.setFriction(1,1);
         
-        this.setBodySize(this.body.width-6,this.body.height-30);
+        this.setBodySize(this.body.width-6,this.body.height-10);
         this.setOffset(3, 10);
         this.jumping = false;  
         
