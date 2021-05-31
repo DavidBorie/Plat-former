76322ef1506e466bd50f89ad4c1319e2bee2ff4b
DavidBowieRie <72392653+DavidBorie@users.noreply.github.com>
2021-05-31 12:52:06 +0200

    Add files via upload

---
+++ assets/toto/Tuiles/Devant.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Devant" tilewidth="32" tileheight="32" tilecount="6090" columns="203">
+ <image source="../../Pictures/SPOING/Calques/Devant.png" width="6500" height="960"/>
+</tileset>
